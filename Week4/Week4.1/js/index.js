d668c804cacc10dad08341adb7fff05343995479
Alex-V-Huang <huang.alex.mx@gmail.com>
2020-08-11 17:25:54 -0700

    add slideshow exercise

---
+++ Week4/Week4.1/js/index.js
@@ -0,0 +1,20 @@
+// Only allows use of declared variables
+'use strict';
+
+// variable dedicated to current displayed image index
+
+function slideshow() {
+
+    // Select all of the images in the slideshow
+
+    // Hide all images from displaying on the page
+
+    // Increase current displayed image index by 1
+
+    // If all of the images have been displayed, display the first image
+
+    // Display the current image on the page
+
+    // Wait two seconds before displaying the next image
+
+}
